bb76759d35cd07f8f9a9c18ae354e28ee6b39ebf
Victor Batigin <bixs85@gmail.com>
2020-02-10 21:01:17 +0200

    add task 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,39 @@
 console.log(`Task 3`);
+
+class Storage {
+  constructor(item) {
+    this.items = item;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    return this.items.push(item);
+  }
+
+  removeItem(item) {
+    if (this.items.includes(item)) {
+      const index = this.items.indexOf(item);
+      this.items.splice(index, 1);
+    }
+    return this.items;
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
