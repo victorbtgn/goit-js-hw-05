7a61d1abc2dac5fb173c9f72b06eb87bda9a2927
Victor Batigin <bixs85@gmail.com>
2020-02-10 21:25:46 +0200

    add task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,34 @@
 console.log(`Task 4`);
+
+class StringBuilder {
+  constructor(str) {
+    this._value = str;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+    return (this._value = this._value + str);
+  }
+
+  prepend(str) {
+    return (this._value = str + this._value);
+  }
+
+  pad(str) {
+    return (this._value = str + this._value + str);
+  }
+}
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
